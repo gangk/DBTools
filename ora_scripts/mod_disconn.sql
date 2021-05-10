83cdf4948a94eb40987acea7445a53a687977f47
Gangadhar K <gangadhar.kreddy@gmail.com>
2021-05-10 11:43:21 -0700

    Delete mod_disconn.sql

--- ora_scripts/mod_disconn.sql
+++
@@ -1,2 +0,0 @@
-select to_char(logoff_time,'DD-MM HH24:MI'),MODULE,count(1) no_of_disconnects from admin.DB_LOGONS where LOGOFF_TIME between to_date ('&start_time','DD-MM-YY HH24:MI') and
-to_date ('&end_time','DD-MM-YY HH24:MI') and module='&module' group by to_char(logoff_time,'DD-MM HH24:MI'),MODULE order by 1;
