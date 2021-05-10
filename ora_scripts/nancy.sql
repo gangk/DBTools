ddcb4d1b03ea20176bf43db7ad4c935ee78782d8
Gangadhar K <gangadhar.kreddy@gmail.com>
2021-05-10 11:45:00 -0700

    Delete nancy.sql

--- ora_scripts/nancy.sql
+++
@@ -1,5 +0,0 @@
-select warehouse_id,scannable_id from containers where container_type = 'WORK_AREA' 
-and scannable_id not like '%A'
-and scannable_id not like '%B'
-and scannable_id not like '%a'
-and scannable_id not like '%b';
