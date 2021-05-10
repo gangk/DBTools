1fe6df72c2f8d78119eca9993ea06ff341345505
Gangadhar K <gangadhar.kreddy@gmail.com>
2021-05-10 11:28:06 -0700

    Delete Kalk_eod.sql

--- ora_scripts/Kalk_eod.sql
+++
@@ -1,8 +0,0 @@
-col time for a20
-
-select a.process_code,b.process,TO_CHAR(a.process_date,'dd-mm-yy HH24:mi:ss') TIME
-from kalkulus.process_log a, kalkulus.codes_process b 
-where a.process_code<160
-and a.process_code=b.process_code
-order by a.process_date;
-
